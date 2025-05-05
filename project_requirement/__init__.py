3c89399b114b1bc11ae006752d698c202db35021
WaelZEIN6 <zeinwael05@gmail.com>
2025-05-05 16:15:31 +0000

    Update _init_.py

--- project_requirement/__init__.py
+++ project_requirement/__init__.py
@@ -2,14 +2,12 @@ from . import models
 from . import wizards
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """
     Post-init hook to ensure the generic department exists.
     This is called when the module is installed.
     """
-    from odoo import api, SUPERUSER_ID
     from .models.common_projects import get_generic_department
 
-    env = api.Environment(cr, SUPERUSER_ID, {})
     # Ensure generic department exists
     get_generic_department(env)
